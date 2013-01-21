a63ea6011ffbc31896f28deeb48ae96dbee14481
Adam Lindqvist <linkaan_@hotmail.com>
2013-01-21 22:11:35 +0100

    Splash screen
    Efter första labben. En splashscreen

---
+++ Quizprojekt/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quizprojekt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Standardvärden för textboxarna
+        string anvStand = "Användarnamn";
+        string passStand = "Password";
+
+        // Sätter värdet i textboxen till empty
+        private void txtbox_Password_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtbox_Password.Password = String.Empty;
+            txtbox_Password.GotFocus -= txtbox_Password_LostFocus;
+        }
+
+
+        // Lite annan skit!!
+        private void txtbox_Password_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtbox_Password.Password != "")
+                passStand = txtbox_Password.Password;
+
+            if (txtbox_Password.Password == "")
+                txtbox_Password.Password = passStand;
+        }
+
+        private void txtbox_Anv_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtbox_Anv.Text == "Användarnamn")
+            txtbox_Anv.Text = String.Empty;
+
+            txtbox_Anv.GotFocus -= txtbox_Anv_LostFocus;
+        }
+
+        private void txtbox_Anv_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtbox_Anv.Text != "")
+                anvStand = txtbox_Anv.Text;
+
+            if (txtbox_Anv.Text == "")
+                txtbox_Anv.Text = anvStand;
+        }
+
+        private void btn_LoggaIn_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void btn_LoggaIn_Click(object sender, RoutedEventArgs e)
+        {
+            Meny menyWin = new Meny();
+            menyWin.Show();
+            this.Close();
+        }
+
+
+    }
+}
---
+++ Quizprojekt/Meny.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Quizprojekt
+{
+    /// <summary>
+    /// Interaction logic for Meny.xaml
+    /// </summary>
+    public partial class Meny : Window
+    {
+        public Meny()
+        {
+            InitializeComponent();
+        }
+    }
+}
