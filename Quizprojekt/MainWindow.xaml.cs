ed5eac845cddee397a35274a5c74f8ed215e7c56
Andreas <zomx-@hotmail.com>
2013-01-24 10:38:39 +0100

    fel mapp

--- Quizprojekt/MainWindow.xaml.cs
+++ Quizprojekt/MainWindow.xaml.cs
@@ -27,54 +27,59 @@ namespace Quizprojekt
         // Standardvärden för textboxarna
         string anvStand = "Användarnamn";
         string passStand = "Password";
-
-        // Sätter värdet i textboxen till empty
-        private void txtbox_Password_GotFocus(object sender, RoutedEventArgs e)
-        {
-            txtbox_Password.Password = String.Empty;
-            txtbox_Password.GotFocus -= txtbox_Password_LostFocus;
-        }
-
-
-        // Lite annan skit!!
-        private void txtbox_Password_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (txtbox_Password.Password != "")
-                passStand = txtbox_Password.Password;
-
-            if (txtbox_Password.Password == "")
-                txtbox_Password.Password = passStand;
-        }
-
+    
         private void txtbox_Anv_GotFocus(object sender, RoutedEventArgs e)
         {
+            // Sätter värdet i Användarnamn-textbox till empty om texten är "Användarnamn"
             if (txtbox_Anv.Text == "Användarnamn")
             txtbox_Anv.Text = String.Empty;
 
+            // När man släpper focus hoppar man till metoden txtbox_Anv_LostFocus
             txtbox_Anv.GotFocus -= txtbox_Anv_LostFocus;
         }
 
         private void txtbox_Anv_LostFocus(object sender, RoutedEventArgs e)
         {
+            // Om Användarnamnboxen INTE är tom sätts anvStand till det som står där i
             if (txtbox_Anv.Text != "")
                 anvStand = txtbox_Anv.Text;
 
+            // Om Passwordboxen är tom sätts texten i boxen till stringen anvStand
             if (txtbox_Anv.Text == "")
                 txtbox_Anv.Text = anvStand;
         }
-
-        private void btn_LoggaIn_MouseUp(object sender, MouseButtonEventArgs e)
+        
+        private void txtbox_Password_GotFocus(object sender, RoutedEventArgs e)
         {
-            
+            // Sätter värdet i Password-textbox till empty
+            txtbox_Password.Password = String.Empty;
+            // När man släpper focus hoppar man till metoden txtbox_Password_LostFocus
+            txtbox_Password.GotFocus -= txtbox_Password_LostFocus;
+        }
+
+        private void txtbox_Password_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // Om Passwordboxen INTE är tom sätts passStand till det som står där i
+            if (txtbox_Password.Password != "")
+                passStand = txtbox_Password.Password;
+            // Om Passwordboxen är tom sätts texten i boxen till stringen passStand
+            else if (txtbox_Password.Password == "")
+                txtbox_Password.Password = passStand;
         }
 
         private void btn_LoggaIn_Click(object sender, RoutedEventArgs e)
         {
+            // Öppnar ett nytt Meny.xaml-fönster
             Meny menyWin = new Meny();
             menyWin.Show();
             this.Close();
         }
 
+        private void btn_BliMedlem_Click(object sender, RoutedEventArgs e)
+        {
+            bliMedlem medlemWin = new bliMedlem();
+            medlemWin.Show();
 
+        }
     }
 }
