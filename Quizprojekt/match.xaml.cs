b78bf51e375d8903ecf2e41ec84308eb9e686020
Adam Lindqvist <adamlindqvist@outlook.com>
2013-01-28 15:52:43 +0100

    Klart för dagen, Svaren kollas, Frågor slumpas

--- Quizprojekt/match.xaml.cs
+++ Quizprojekt/match.xaml.cs
@@ -47,7 +47,8 @@ namespace Quizprojekt
 
             OleDbCommand myOleDbCommand = myOleDbConnection.CreateCommand();
 
-            myOleDbCommand.CommandText = "SELECT * FROM Fragor WHERE KategoriID = 5";
+            //Query för att hämta alla för en viss kategori
+            myOleDbCommand.CommandText = "SELECT * FROM Fragor WHERE KategoriID = 4";
             myOleDbConnection.Open();
 
             OleDbDataReader myOleDbDataReader = myOleDbCommand.ExecuteReader();
@@ -56,36 +57,35 @@ namespace Quizprojekt
 
             List<string> idList = new List<string>(); // Deklarerar en lista.
 
+            int antalID = 0;
             // Läser alla rader i databasen med kommandot givet ovan.
             while (myOleDbDataReader.Read())
             {
-
                 // Lägger till ett ID i idList.
                 idList.Add(Convert.ToString(myOleDbDataReader["FragorID"]));
+                antalID++;
             }
 
             Random random = new Random();
-            int num = random.Next(10);
+            int num = random.Next(antalID);
 
+            //Tar FragorID på positionen num
             idFraga = idList.ElementAtOrDefault(num);
 
-
             setQuestion();
-
         }
-
 
 
         // Skriver ut frågan + svarsalternativen
         private void setQuestion()
         {
-
             string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=BrainiestDB.accdb";
 
             OleDbConnection myOleDbConnection = new OleDbConnection(connectionString);
 
             OleDbCommand myOleDbCommand = myOleDbConnection.CreateCommand();
 
+            //Query för att hämta allt från en viss fråga
             myOleDbCommand.CommandText = "SELECT * FROM Fragor WHERE FragorID = " + idFraga;
             myOleDbConnection.Open();
 
@@ -98,15 +98,14 @@ namespace Quizprojekt
             txtbox_Fraga.Text = fraga;
 
             string korrekt = Convert.ToString(myOleDbDataReader["Korrekt"]);
-            btn_Svar1.Content = korrekt;
-
             string fel1 = Convert.ToString(myOleDbDataReader["Fel1"]);
-            btn_Svar2.Content = fel1;
-
             string fel2 = Convert.ToString(myOleDbDataReader["Fel2"]);
-            btn_Svar3.Content = fel2;
-
             string fel3 = Convert.ToString(myOleDbDataReader["Fel3"]);
+
+            // DESSA SKA SLUMPAS ***********FIXAS*************
+            btn_Svar1.Content = korrekt;
+            btn_Svar2.Content = fel1;
+            btn_Svar3.Content = fel2;
             btn_Svar4.Content = fel3;
 
             myOleDbDataReader.Close();
@@ -114,10 +113,65 @@ namespace Quizprojekt
         }
 
 
+
         // När tiden gått ut i progressbaren:
         private void progressBarAnimation_Completed(object sender, EventArgs e)
         {
             MessageBox.Show("Tiden är ute.");
+        }
+
+        //Beroende på vad man svarat så skickas denna text till checkAnswer()
+        private void btn_Svar1_Click(object sender, RoutedEventArgs e)
+        {
+            string content = Convert.ToString(btn_Svar1.Content);
+            checkAnswer(content);
+        }
+
+        private void btn_Svar2_Click(object sender, RoutedEventArgs e)
+        {
+            string content = Convert.ToString(btn_Svar2.Content);
+            checkAnswer(content);
+        }
+
+        private void btn_Svar3_Click(object sender, RoutedEventArgs e)
+        {
+            string content = Convert.ToString(btn_Svar3.Content);
+            checkAnswer(content);
+        }
+
+        private void btn_Svar4_Click(object sender, RoutedEventArgs e)
+        {
+            string content = Convert.ToString(btn_Svar4.Content);
+            checkAnswer(content);
+        }
+
+        //Kollar om ditt svar och det rätta svaret är samma
+        private void checkAnswer(string btnAnswer)
+        {
+
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=BrainiestDB.accdb";
+
+            OleDbConnection myOleDbConnection = new OleDbConnection(connectionString);
+
+            OleDbCommand myOleDbCommand = myOleDbConnection.CreateCommand();
+
+            myOleDbCommand.CommandText = "SELECT Korrekt FROM Fragor WHERE FragorID = " + idFraga;
+            myOleDbConnection.Open();
+
+            OleDbDataReader myOleDbDataReader = myOleDbCommand.ExecuteReader();
+
+
+            myOleDbDataReader.Read();
+            
+            string svar = Convert.ToString(myOleDbDataReader["Korrekt"]);
+
+            if (svar == btnAnswer)
+                MessageBox.Show("RÄTT");
+            else
+                MessageBox.Show("FEL");
+
+
+            readQuestion();
         }
 
     }
