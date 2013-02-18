2795a5eb8844cd048ce21e8e2376ff6e0b850a89
Andreas <zomx-@hotmail.com>
2013-02-18 15:17:51 +0100

    Slumpa motståndare funkar halvt

--- Quizprojekt/NyttSpel.xaml.cs
+++ Quizprojekt/NyttSpel.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace Quizprojekt
 {
@@ -34,6 +35,38 @@ namespace Quizprojekt
         {
             Switcher.Switch(new Meny());
         }
+
+        private void btn_Slumpa_Click(object sender, RoutedEventArgs e)
+        {
+            DBconnect.openDB("SELECT * FROM Medlemmar ORDER BY rand() LIMIT 1");
+            DBconnect.DataReader.Read();
+            txtbox_Sok.Text = Convert.ToString(DBconnect.DataReader["MedlemmarID"]);
+            DBconnect.Connection.Close();
+        }
+
+        private void btn_Soek_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string soekAnv = txtbox_Sok.Text;
+
+                DBconnect.openDB("SELECT * FROM Medlemmar WHERE MedlemmarID = " + soekAnv);
+                while (DBconnect.DataReader.Read())
+                {
+
+                    listBox1.Items.Add(Convert.ToString(DBconnect.DataReader["Anvandarnamn"]));
+
+                }
+
+                DBconnect.Connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
 
     }
 }
